39fa42e32e785ca83457c3ebc5611bc6aeb545b6
Gustav <gustavhlarsen01@gmail.com>
2022-09-19 11:14:58 +0200

    Update nvim_install_script.sh

--- bashScripts/nvim_install_script.sh
+++ bashScripts/nvim_install_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # install nvim ppa for latest stable version.
-sudo add-apt-repository ppa:neovim-ppa/stable;
-sudo apt update;
+sudo add-apt-repository --yes ppa:neovim-ppa/stable;
+sudo apt-get -y update; sudo apt-get -y upgrade
 # install neovim.
 sudo apt -y install neovim;
 # download and install config
 cd ~/.config/;
 git clone https://github.com/gustavhlarsen01/nvim;
 cd ~;
+# Convert dos config to unix, if the config is from MS Windows.
+sudo apt-get -y install dos2unix;
+dos2unix ~/.config/nvim/init.vim;
